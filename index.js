8200c936297dc905c4cd64ee8931c4db0bab3fa9
xarita <128380509+candemir22@users.noreply.github.com>
2025-12-13 01:59:26 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,51 @@
-let featureLayer;
 let marker;
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 13,
+    center: { lat: 42.325, lng: 42.07 },
+  });
 
-async function initMap() {
+	  const triangleCoords = [
+    { lat: 42.774, lng: 42.19 },
+    { lat: 42.466, lng:42.118 },
+    { lat: 42.321, lng: 42.757 },
+    { lat: 42.774, lng: 42.19 },
+  ];
+  // Construct the polygon.
+  const bermudaTriangle = new google.maps.Polygon({
+    paths: triangleCoords,
+    strokeColor: "#FF0000",
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: "#FF0000",
+    fillOpacity: 0.35,
+  });
+  bermudaTriangle.setMap(map);
 
-    await google.maps.importLibrary("maps");
-
-    const mapElement = new google.maps.Map(document.getElementById("map"), {    zoom: 18,    center: { lat: 37.214931, lng: 28.363885 },    mapTypeId: "terrain",  });
-    // Get the inner map.
-    const innerMap = mapElement.innerMap;
-    // Get the feature layer.
-    featureLayer = innerMap.getFeatureLayer(google.maps.FeatureType.LOCALITY);
-    // Define a style with purple fill and border.
-    
-     marker = new google.maps.Marker({
-    gmp-map,
+  marker = new google.maps.Marker({
+    map,
     draggable: true,
     animation: google.maps.Animation.DROP,
     position: { lat: 42.327, lng: 42.067 },
-  }); 
-    
+  });
 
-
-
-
-
-    
+    marker.addListener("mouseover", kazibol);
+ marker.addListener("click", cikis);
 }
 
-initMap();
+function kazibol() {
+  if (marker.getAnimation() !== null) {
+    marker.setAnimation(null);
+  } else {
+    	window.speechSynthesis.speak(new SpeechSynthesisUtterance('kazı bölgesi '));
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  
+  }}
 
+function cikis() {
+     marker.setAnimation(null);
+     	window.speechSynthesis.speak(new SpeechSynthesisUtterance('bu bölgenin proje kodu 128.60'));  
+		}
 
-
-
-
-
+  
+window.initMap = initMap;
